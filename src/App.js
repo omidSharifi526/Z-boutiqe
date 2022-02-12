1b146c581c224b212baa0d8eabdaa15d651217d5
sharifidavood <sharifidavood743@gmail.com>
2022-02-12 14:16:18 -0800

    first-com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Img from '../src/img/maria-lupan-gS_pSZHbWj4-unsplash.jpg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        create project
-      </header>
-    </div>
+  <div className='main-cont'>
+<img src={Img}/>
+  </div>
   );
 }
 
